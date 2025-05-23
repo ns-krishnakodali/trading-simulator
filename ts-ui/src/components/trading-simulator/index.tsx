cc033e0cb6df98656e47c00a74379c181d958269
Krishna Kodali <krishnakodali81@gmail.com>
2025-05-23 18:01:27 -0400

    new: setup layout and add icons

---
+++ ts-ui/src/components/trading-simulator/index.tsx
@@ -0,0 +1,7 @@
+export const TradingSimulator = () => {
+  return (
+    <div className="flex justify-center pt-6">
+      <h1 className="text-4xl font-bold text-gray-800">Trading Simulator</h1>
+    </div>
+  );
+};
